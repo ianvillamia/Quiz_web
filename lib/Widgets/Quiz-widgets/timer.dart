81dd53b29cbc0985d91f70a3af66161850e2036a
ianvillamia <ianvillamia0123@gmail.com>
2020-04-07 21:35:36 +0800

    ETO UN DOI

--- lib/Widgets/Quiz-widgets/timer.dart
+++ lib/Widgets/Quiz-widgets/timer.dart
@@ -10,60 +10,58 @@ class Timer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _quizProvider = Provider.of<QuizProvider>(context);
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(color:Colors.black),
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(15), bottomLeft: Radius.circular(15))),
-      width: 250,
-      height: 150,
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: 75,
-            child: Visibility(
-              visible: _quizProvider.isTimerVisible,
-              child: SlideCountdownClock(
-                duration: Duration(minutes: 1),
-                slideDirection: SlideDirection.Up,
-                separator: "-",
-                textStyle: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+    return Card(
+          child: Container(
+      
+        width: 250,
+        height: 150,
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 75,
+              child: Visibility(
+                visible: _quizProvider.isTimerVisible,
+                child: SlideCountdownClock(
+                  duration: Duration(minutes: 1),
+                  slideDirection: SlideDirection.Up,
+                  separator: "-",
+                  textStyle: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  separatorTextStyle: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(color: Colors.redAccent),
+                  onDone: () {},
                 ),
-                separatorTextStyle: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue,
-                ),
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(color: Colors.redAccent),
-                onDone: () {},
               ),
             ),
-          ),
-          LiteRollingSwitch(
-            //initial value
-            value: _quizProvider.isTimerVisible,
-            textOn: 'Timer on',
-            textOff: 'Timer off',
-            colorOn: Colors.greenAccent[700],
-            colorOff: Colors.redAccent[700],
-            iconOn: Icons.alarm_on,
-            animationDuration: Duration(milliseconds: 500),
-            iconOff: Icons.alarm_off,
-            textSize: 10.0,
-            onChanged: (bool state) {
-              //WEIRD TINATAWAG SYA ON INIT BALUGA
-              if (_quizProvider.initCounter != 1) {
-                _quizProvider.changeTimer(state);
-              }
-              _quizProvider.initCounter = 2;
-            },
-          ),
-        ],
+            LiteRollingSwitch(
+              //initial value
+              value: _quizProvider.isTimerVisible,
+              textOn: 'Timer on',
+              textOff: 'Timer off',
+              colorOn: Colors.greenAccent[700],
+              colorOff: Colors.redAccent[700],
+              iconOn: Icons.alarm_on,
+              animationDuration: Duration(milliseconds: 500),
+              iconOff: Icons.alarm_off,
+              textSize: 10.0,
+              onChanged: (bool state) {
+                //WEIRD TINATAWAG SYA ON INIT BALUGA
+                if (_quizProvider.initCounter != 1) {
+                  _quizProvider.changeTimer(state);
+                }
+                _quizProvider.initCounter = 2;
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
